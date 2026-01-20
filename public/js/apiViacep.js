4286d6fc5482b6ded24ee5cf7af97c5db2383855
MiltonJR7 <milton.oliveira.jr777@gmail.com>
2026-01-20 14:07:13 -0300

    Aplicando APi do viacep para validar cep existente

---
+++ public/js/apiViacep.js
@@ -0,0 +1,49 @@
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    const btnLimpar = document.getElementById('btnLimparCampo');
+    const cepInput = document.getElementById('cepInput');
+    btnLimpar.addEventListener('click', limpa_formulário_cep);
+
+    cepInput.addEventListener('keyup', (event)=> {
+        const valor = document.getElementById('cepInput').value;
+        var cep = valor.replace(/\D/g, '');
+
+        if(cep !== "") {
+            var validacep = /^[0-9]{8}$/;
+            if(event.key === 'Enter') {
+                if(validacep.test(cep)) {
+                    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+                    .then((res)=> {
+                        return res.json();
+                    })
+                    .then((corpo)=> {
+                        if(!corpo.erro) {
+                            document.getElementById('logradouroInput').value=(corpo.logradouro);
+                            document.getElementById('bairroInput').value=(corpo.bairro);
+                            document.getElementById('cidadeInput').value=(corpo.localidade);
+                            document.getElementById('estadoInput').value=(corpo.uf);
+                        } else {
+                            limpa_formulário_cep();
+                            alert("CEP não encontrado.");
+                        }
+                    })
+                    .catch((err)=> {
+                        return alert('Error interno, tente novamente mais tarde.');
+                    })
+                } else {
+                    return alert('ERRO: CEP INVALIDO');
+                }
+            }
+        } else {
+            return alert('ERRO: CEP VAZIO');
+        }
+    })
+    
+    function limpa_formulário_cep() {
+        document.getElementById('cepInput').value=("");
+        document.getElementById('logradouroInput').value=("");
+        document.getElementById('bairroInput').value=("");
+        document.getElementById('cidadeInput').value=("");
+        document.getElementById('estadoInput').value=("");
+    }
+})
